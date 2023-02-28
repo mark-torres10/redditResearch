08411f0a3c8483e0424e36cf9c7b089705f6c9a5
mark-torres10 <markptorres1@gmail.com>
2023-02-28 16:26:28 +0800

    write function

--- src/lib/helper.py
+++ src/lib/helper.py
@@ -6,6 +6,5 @@ ROOT_DIR = "/Users/mark/Documents/work/redditResearch/"
 
 def write_dict_list_to_csv(dict_list: List[Dict], write_path: str) -> None:
     """Given a list of dictionaries, dump the data to .csv."""
-    # step 1: Convert list of dictionaries to a Pandas dataframe.
-    # step 2: Convert Pandas dataframe to .csv. Save to filesystem.
-    pass
+    df = pd.DataFrame(dict_list)
+    df.to_csv(write_path, index=False)
