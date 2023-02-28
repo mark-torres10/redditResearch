017d3a095d4dea7452e50f3361d5e492522cc8f1
mark-torres10 <markptorres1@gmail.com>
2023-02-28 16:35:01 +0800

    black formatting

--- src/lib/helper.py
+++ src/lib/helper.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 ROOT_DIR = "/Users/mark/Documents/work/redditResearch/"
 
+
 def write_dict_list_to_csv(dict_list: List[Dict], write_path: str) -> None:
     """Given a list of dictionaries, dump the data to .csv."""
     df = pd.DataFrame(dict_list)
--- src/lib/tests/helper_test.py
+++ src/lib/tests/helper_test.py
@@ -1,24 +1,27 @@
 from tempfile import NamedTemporaryFile
+from typing import Dict, List
 
 import pandas as pd
 
 from lib.helper import write_dict_list_to_csv
 
+
 class TestWriteDictListToCsv:
     """Tests the `write_dict_list_to_csv function`."""
+
     @staticmethod
-    def create_test_data():
+    def create_test_data() -> List[Dict]:
         return [
-            {'name': 'Alice', 'age': 25, 'location': 'New York'},
-            {'name': 'Bob', 'age': 30, 'location': 'San Francisco'},
-            {'name': 'Charlie', 'age': 35, 'location': 'Boston'}
+            {"name": "Alice", "age": 25, "location": "New York"},
+            {"name": "Bob", "age": 30, "location": "San Francisco"},
+            {"name": "Charlie", "age": 35, "location": "Boston"},
         ]
 
-    def test_output_file(self):
+    def test_output_file(self) -> None:
         """Tests that the output file is created."""
         data = self.create_test_data()
 
-        with NamedTemporaryFile(mode='w', delete=False) as f:
+        with NamedTemporaryFile(mode="w", delete=False) as f:
             filename = f.name
 
             write_dict_list_to_csv(data, filename)
@@ -26,11 +29,11 @@ class TestWriteDictListToCsv:
             df = pd.read_csv(filename)
             assert len(df) == len(data)
 
-    def test_output_content(self):
+    def test_output_content(self) -> None:
         """Tests that the output content is correct."""
         data = self.create_test_data()
 
-        with NamedTemporaryFile(mode='w', delete=False) as f:
+        with NamedTemporaryFile(mode="w", delete=False) as f:
             filename = f.name
 
             write_dict_list_to_csv(data, filename)
