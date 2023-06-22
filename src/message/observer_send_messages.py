7c5abf7dc1c6ad6670f14d489cc7a3d906cf90fc
mark-torres10 <markptorres1@gmail.com>
2023-06-22 15:02:06 +0800

    start skeleton of observer phase

---
+++ src/message/observer_send_messages.py
@@ -0,0 +1,23 @@
+"""Implement observer phase of research."""
+import sys
+from typing import List
+
+def determine_which_posts_to_use(sync_timestamp: str):
+    pass
+
+
+def determine_users_to_message() -> List[str]:
+    pass
+
+
+def create_observer_message_dm() -> str:
+    pass
+
+
+def send_observer_messages() -> str:
+    pass
+
+
+if __name__ == "__main__":
+    load_timestamp = sys.argv[1]
+    pass
