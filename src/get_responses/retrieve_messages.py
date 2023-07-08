2ad43fa2db8b3a8feb81a23de91d39da711565b7
mark-torres10 <markptorres1@gmail.com>
2023-07-08 19:22:28 +0800

    label current responses

--- src/get_responses/retrieve_messages.py
+++ src/get_responses/retrieve_messages.py
@@ -9,10 +9,11 @@ the date that our DM was sent, and the date that we received the response.
 import datetime
 import os
 import re
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 import pandas as pd
 import praw
+from praw.models.reddit.message import Message
 
 from get_responses import constants
 from lib.reddit import init_api_access
@@ -68,15 +69,10 @@ class RedditMessage:
             message.created_utc
         )
         self.replies: List[praw.models.Message] = message.replies
-        self.possibly_has_valid_response = (
-            self.check_if_possible_valid_response(self.response_body)
-        )
 
+    def __repr__(self):
+        return f"Message: {self.response_body}"
 
-    def __dict__(self):
-        """Converts attributes into dictionary that we can convert into
-        a pandas df."""
-        pass
 
     @classmethod
     def check_if_possible_valid_response(self, response: str) -> bool:
@@ -91,146 +87,88 @@ class RedditMessage:
         return pd.DataFrame(message_lst)
     
 
-def get_sent_messages(api: praw.Reddit) -> List[RedditMessage]:
-    """Get the messages that we've sent to users."""
-    sent_messages = [RedditMessage(msg) for msg in api.inbox.sent(limit=None)]
-    print(f"Number of sent messages: {len(sent_messages)}")
-    return sent_messages
+class ResponseObject:
+    """Wrapper class to contain all the information about a response that
+    we want to track"""
+    pass
 
 
-def get_sent_messages_with_responses(api: praw.Reddit) -> List[RedditMessage]:
-    """Get the messages that we've sent to users that have responses."""
-    sent_messages = get_sent_messages(api)
-    return [msg for msg in sent_messages if len(msg.replies) > 0]
-
-
-def get_messages_with_possibly_valid_responses(
-    api: praw.Reddit
-) -> List[RedditMessage]:
-    """Get the messages that we've sent to users that have responses."""
-    sent_messages = get_sent_messages(api)
-    return [msg for msg in sent_messages if msg.possibly_has_valid_response]
-
-
-def get_possibly_valid_responses(
-    messages_with_possible_responses: List[RedditMessage]
-) -> List[Dict]:
-    """Returns a dictionary where the keys indicate the original DM that
-    we sent as well as the user's response.
+def get_messages_received(api: praw.Reddit) -> List[Message]:
+    """Get the messages that we have received from other people.
     
-    Key is the message ID and value is a dictionary of the original DM, original
-    DM object, and the response message."""
-    responses = {}
-    for msg in messages_with_possible_responses:
-        replies = msg.replies
-        replies_body_list = []
-        for reply in replies:
-            replies_body_list.append(reply.body)
-        replies_body = '\n'.join(replies_body_list)
-        responses[msg.message_id] = {
-            "original_message_object": msg,
-            "original_dm": msg.response_body,
-            "response_dm": replies_body
-        }
-    return responses
+    This assumes that people only DM us if we have DMed them first.
+    """
+    messages_received = [msg for msg in api.inbox.messages(limit=None)]
+    return [
+        msg.replies[0] for msg in messages_received if len(msg.replies) > 0
+    ]
 
 
-def manually_validate_responses(
-    previously_labeled_ids: List[str],
-    possibly_valid_responses: List[Dict]
-) -> None:
-    """Given the list of possibly valid responses, validate those responses
-    manually, add to a pandas df, and write to a .csv file."""
-    validated_responses = []
-    total_responses = len(possibly_valid_responses)
-    for idx, (message_id, message_obj) in enumerate(
-        possibly_valid_responses.items()
-    ):
-        print('-' * 5)
-        print(f"Validating response {idx} out of {total_responses} responses.")
+def load_previously_labeled_ids() -> List[str]:
+    previously_labeled_data = []
+    load_dir = constants.VALIDATED_RESPONSES_ROOT_PATH
+    for filepath in os.listdir(load_dir):
+        df = pd.read_csv(os.path.join(load_dir, filepath))
+        ids = df["message_id"].tolist()
+        previously_labeled_data.extend(ids)
 
-        message = message_obj["original_message_object"]
-        response_dm = message_obj["response_dm"]
+    return previously_labeled_data
 
-        print(f"Response body: {response_dm}")
 
-        if message_id in previously_labeled_ids:
-            print(f"Skipping {message_id} since it has already been labeled previously.")  # noqa
+def write_labels_to_csv(labels: List[Tuple[str, int, str]]) -> None:
+    colnames = ["message_id", "is_valid_response", "scores"]
+    df = pd.DataFrame(labels, columns=colnames)
+    df.to_csv(SESSION_VALIDATED_RESPONSES_FILEPATH)
+
+
+def manually_validate_messages(messages: List[Message]) -> List[Message]:
+    """Validates the messages that we have received.
+    
+    Manually QAs the messages and records the ones that have valid responses.
+
+    Writes the IDs
+    """
+    previously_labeled_data_ids = load_previously_labeled_ids()
+
+    num_messages = len(messages)
+    responses_list: List[Tuple[str, int, str]] = []
+
+    for idx, msg in enumerate(messages):
+        print('-' * 10)
+        print(f"Labeling message {idx} out of {num_messages}")
+        # print the body
+        print(f"Body: {msg.body}")
+        # ask if valid (y/n) or if to exit session
+        user_input = ''
+        valid_inputs = ['y', 'n', 'e']
+        break_session = False
+        if msg.id in previously_labeled_data_ids:
+            print(f"Previously labeled response, with id {msg.id}, skipping")
             continue
-        check = ''
-        valid_checks = ['y', 'n', 'q']
-        while check not in valid_checks:
-            base_response_obj = {
-                "message_id": message_id,
-                "author_name": message.author_name,
-                "author_t2_id": message.author_t2_id,
-                "subject": message.subject,
-                "response_body": message.response_body,
-                "created_utc_string": message.created_utc_string
-            }
-            check = input("Is this a valid response? (y=yes, n=no, q=quit)")
-            if check == 'y':
-                validated_score = input(
-                    "What are their scores? Give as 4 digit string"
-                )
-                updated_response_obj = {
-                    **base_response_obj,
-                    **{
-                        "is_valid_response": True,
-                        "validated_score": validated_score
-                    }
-                }
-                validated_responses.append(updated_response_obj)
-                check = ''
-                continue
-            if check == 'n':
-                updated_response_obj = {
-                    **base_response_obj,
-                    **{
-                        "is_valid_response": False,
-                        "validated_score": None
-                    }
-                }
-                validated_responses.append(updated_response_obj)
-                print(f"{response_dm} not valid response, skipping...")
-                check = ''
-                continue
-            if check == 'q':
-                print(f"Quitting labeling session. QAed {idx} out of {total_responses}") # noqa
-                break
+        while user_input not in valid_inputs:
+            user_input = input("Is this a valid response? ('y', 'n', or 'e' to exit)\t") # noqa
+            if user_input == 'y':
+                print("Valid response.")
+                scores = input("Please enter their scores (e.g., 1123):\t")
+                responses_list.append((msg.id, 1, scores))
+
+            elif user_input == 'n':
+                print("Invalid response")
+                responses_list.append((msg.id, 0, ''))
+            elif user_input == 'e':
+                print("Exiting labeling session...")
+                break_session = True
             else:
-                print(f"Please provide a valid input. {check} not in [{valid_checks}]") # noqa
-    print(f"Validated {total_responses} possibly valid responses.")
+                print(f"Invalid input: {user_input}")
+        user_input = ''
+        if break_session:
+            break
 
-    # create and dump file of today's validation session.
-    print(
-        f"Writing to df and dumping to .csv file at {SESSION_VALIDATED_RESPONSES_FILEPATH}" # noqa
-    )
-    validated_responses_df = pd.DataFrame(validated_responses)
-    validated_responses_df.to_csv(SESSION_VALIDATED_RESPONSES_FILEPATH)
-
-    # create and dump file of all validation sessions.
-    print(f"Consolidating data from all labeling sessions into one .csv file at {ALL_VALIDATED_RESPONSES_FILEPATH}")
-    all_responses = previously_labeled_data + validated_responses
-    all_responses_df = pd.DataFrame(all_responses)
-    all_responses_df.to_csv(ALL_VALIDATED_RESPONSES_FILEPATH)
+    write_labels_to_csv(responses_list)
 
 
 if __name__ == "__main__":
-    # go through DMs in inbox
-    possibly_valid_messages = get_messages_with_possibly_valid_responses(api)
-    possibly_valid_responses = get_possibly_valid_responses(
-        messages_with_possible_responses=possibly_valid_messages
-    )
+    messages_received = get_messages_received(api)
+    manually_validate_messages(messages_received)
 
-    # get previously labeled data:
-    previously_labeled_data = get_previous_labeling_session_data()
-    previously_labeled_ids = [
-        data["message_id"] for data in previously_labeled_data
-    ]
 
-    # do manual QA
-    manually_validate_responses(
-        previously_labeled_ids=previously_labeled_ids,
-        possibly_valid_responses=possibly_valid_responses
-    )
