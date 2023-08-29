96d69be8a61042787ba631526d6402fbd413ecac
mark-torres10 <markptorres1@gmail.com>
2023-08-29 16:34:07 +0800

    move files to observer phase

--- src/analysis/perform_analysis.py
+++ src/analysis/perform_analysis.py
@@ -1,2 +1,14 @@
+from analysis import helper
+from lib.reddit import init_api_access
+
+api = init_api_access()
+
+def get_total_number_of_dms_sent() -> None:
+    pass
+
 def generate_analyses() -> None:
+    pass
+
+
+if __name__ == "__main__":
     pass
