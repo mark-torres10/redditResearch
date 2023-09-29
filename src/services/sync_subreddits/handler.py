039950c97f2ab490530f640e151411e4d8d9d7a3
mark-torres10 <markptorres1@gmail.com>
2023-09-29 19:46:46 +0800

    feat: implement 'sync_subreddits'

--- src/services/sync_subreddits/handler.py
+++ src/services/sync_subreddits/handler.py
@@ -2,4 +2,31 @@
 
 Returns list of payloads for syncing each individual subreddit.
 """
-pass
+from services.sync_single_subreddit.helper import (
+    DEFAULT_NUM_THREADS, DEFAULT_NUM_COMMENTS_PER_THREAD
+)
+
+# TODO: implement
+def get_all_subreddits() -> list[str]:
+    return []
+
+
+def main(event: dict, context: dict) -> list[dict]:
+    subreddits = event.get("subreddits", "all")
+    num_threads = event.get("num_threads", DEFAULT_NUM_THREADS)
+    num_comments_per_thread = event.get(
+        "num_comments_per_thread", DEFAULT_NUM_COMMENTS_PER_THREAD
+    )
+    if subreddits == "all":
+        subreddits = get_all_subreddits()
+    else:
+        subreddits = subreddits.split(',')
+    payloads = [
+        {
+            "subreddit": subreddit,
+            "num_threads": num_threads,
+            "num_comments_per_thread": num_comments_per_thread
+        }
+        for subreddit in subreddits
+    ]
+    return payloads
