0e6b6e524f91ab3297aa4b9975fa51df14e3366d
mark-torres10 <markptorres1@gmail.com>
2023-10-19 17:29:22 +0800

    feat: have working implementation of langchain for db query

--- src/pipelines/gpt/main.py
+++ src/pipelines/gpt/main.py
@@ -3,9 +3,10 @@ from lib.helper import track_function_runtime
 from services.query_gpt.handler import main as query_gpt
 
 @track_function_runtime
-def main() -> None:
-    query_gpt()
+def main(event: dict) -> None:
+    query_gpt(event)
 
 
 if __name__ == "__main__":
-    pass
+    event = {"query": "How many comments have we synced?"}
+    main(event)
--- src/services/query_gpt/handler.py
+++ src/services/query_gpt/handler.py
@@ -1,5 +1,6 @@
 """Query ChatGPT using OpenAI API."""
 from services.query_gpt.helper import query_chatgpt
 
-def handler(event: dict, context: dict) -> None:
-    query_chatgpt()
+def main(event: dict, context: dict) -> None:
+    query = event["query"]
+    query_chatgpt(query)
