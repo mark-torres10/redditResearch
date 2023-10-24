831fbbe277e176261e2c0a6029e8259b411002a9
mark-torres10 <markptorres1@gmail.com>
2023-10-24 22:28:45 +0800

    update backup script to use csv

--- src/lib/db/sql/backup_postgres_data.py
+++ src/lib/db/sql/backup_postgres_data.py
@@ -6,12 +6,14 @@ version of the data as well as the schemas of every existing table.
 import gzip
 import json
 import os
+from typing import Optional, Literal
 
 from lib.db.sql.helper import (
     current_file_directory, cursor, get_all_tables_in_db,
-    get_table_col_to_dtype_map
+    get_table_col_to_dtype_map, load_table_as_df
 )
 from lib.helper import CURRENT_TIME_STR
+
 
 def init_directory() -> str:
     backup_dir = os.path.join(current_file_directory, "snapshots")
@@ -24,9 +26,7 @@ def init_directory() -> str:
 
 
 def dump_table_to_sql(
-    table_name: str,
-    table_fp: str,
-    zipped: bool = False
+    table_name: str, table_fp: str, zipped: bool = False
 ) -> None:
     query = f"SELECT * FROM {table_name}"
     copy_query = f"COPY ({query}) TO STDOUT"
@@ -41,19 +41,43 @@ def dump_table_to_sql(
     print(f"Successfully dumped '{table_name}' table to {table_fp}.")
 
 
+def dump_table_to_csv(
+    table_name: str, table_fp: str, zipped: bool = False
+) -> None:
+    df = load_table_as_df(table_name)
+    try:
+        if zipped:
+            df.to_csv(table_fp, index=False, compression="gzip")
+        else:
+            df.to_csv(table_fp, index=False)
+        print(f"Successfully dumped '{table_name}' table to {table_fp}.")
+    except Exception as e:
+        print(f"Unable to dump '{table_name}' table to {table_fp}.")
+        raise e
+
+
 def dump_postgres_db(
-    timestamp_dir: str, table_list: list[str], zipped: bool=False
+    timestamp_dir: str, table_list: list[str], zipped: bool=False,
+    format: Optional[Literal["csv", "sql"]] = "csv"
 ) -> None:
     print(f"Dumping DB tables to {timestamp_dir}...")
-    for table in table_list:
-        suffix = ".sql.gz" if zipped else ".sql"
-        table_fp = os.path.join(timestamp_dir, f"{table}{suffix}")
-        dump_table_to_sql(
-            table_name=table,
-            table_fp=table_fp,
-            zipped=zipped
-        )
-        print(f"Finished dumping {table} table to {table_fp}.")
+    for table_name in table_list:
+        suffix = ".csv" if format == "csv" else ".sql"
+        suffix = suffix + ".gz" if zipped else suffix
+        table_fp = os.path.join(timestamp_dir, f"{table_name}{suffix}")
+        if format == "sql":
+            dump_table_to_sql(
+                table_name=table_name,
+                table_fp=table_fp,
+                zipped=zipped
+            )
+        elif format == "csv":
+            dump_table_to_csv(
+                table_name=table_name,
+                table_fp=table_fp,
+                zipped=zipped
+            )
+
     print(f"Finished successfully dumping tables from DB.")
 
 
@@ -87,7 +111,8 @@ def main() -> None:
     timestamp_dir = init_directory()
     table_list: list[str] = get_all_tables_in_db()
     dump_postgres_db(
-        timestamp_dir=timestamp_dir, table_list=table_list, zipped=True
+        timestamp_dir=timestamp_dir, table_list=table_list, zipped=True,
+        format="csv"
     )
     dump_postgres_db_schemas_to_file(
         timestamp_dir=timestamp_dir, table_list=table_list, zipped=False
