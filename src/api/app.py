5f69bb0e3abccacfc74c42ca5c32646d5fc9a9ee
mark-torres10 <markptorres1@gmail.com>
2023-11-16 16:44:14 -0500

    api wip

--- src/api/app.py
+++ src/api/app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-from services.classify_comments.inference import classify_text
+# from services.classify_comments.inference import classify_text
 
 
 load_dotenv()
@@ -15,10 +15,14 @@ flask_run_host = os.getenv("FLASK_RUN_HOST")
 
 app = Flask(__name__)
 CORS(app)
-app.config["FLASK_APP"] = flask_app
+# app.config["FLASK_APP"] = flask_app
 
 
-@app.route("/api/classify", method=["POST"])
+def classify_text() -> int:
+    return {"prob": 0.9, "label": 1}
+
+
+@app.route("/api/classify", methods=["POST"])
 def classify() -> tuple[dict, int]:
     try:
         data = request.get_json()
@@ -26,11 +30,13 @@ def classify() -> tuple[dict, int]:
             return jsonify({"error": "The 'text' field is missing in the payload"}), 400
         text = data["text"]
         result = classify_text(text)
-        return jsonify(result), 200
+        res = {**result, "text": text}
+        return jsonify(res), 200
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 
 if __name__ == "__main__":
-    app.run(port=int(flask_run_port), host=flask_run_host, debug=True)
+    # app.run(port=int(flask_run_port), host=flask_run_host, debug=True)
+    app.run(debug=True)
